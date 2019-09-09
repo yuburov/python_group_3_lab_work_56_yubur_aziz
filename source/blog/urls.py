06b89ea632d8de1e57b5605b0cd2c98e4c979067
Farid <thesomeq@gmail.com>
2019-09-09 20:06:34 +0600

    Update and delete.

--- source/blog/urls.py
+++ source/blog/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, article_view, article_create_view
+from webapp.views import index_view, article_view, article_create_view,\
+    article_update_view, article_delete_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
     path('article/<int:pk>/', article_view, name='article_view'),
     path('article/add/', article_create_view, name='article_add'),
+    path('article/<int:pk>/edit/', article_update_view, name='article_update'),
+    path('article/<int:pk>/delete/', article_delete_view, name='article_delete')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -26,3 +26,24 @@ def article_create_view(request, *args, **kwargs):
         text = request.POST.get('text')
         article = Article.objects.create(title=title, author=author, text=text)
         return redirect('article_view', pk=article.pk)
+
+
+def article_update_view(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'update.html', context={'article': article})
+    elif request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.author = request.POST.get('author')
+        article.text = request.POST.get('text')
+        article.save()
+        return redirect('article_view', pk=article.pk)
+
+
+def article_delete_view(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'delete.html', context={'article': article})
+    elif request.method == 'POST':
+        article.delete()
+        return redirect('index')
