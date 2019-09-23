ebb442ee0d1bcbff3b9e7876e4fbc4e7786f8bf5
Farid <thesomeq@gmail.com>
2019-09-23 20:37:37 +0600

    Добавлена модель Comment.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -10,3 +10,15 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey('webapp.Article', related_name='comments',
+                                on_delete=models.CASCADE, verbose_name='Статья')
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return self.text[:20]
