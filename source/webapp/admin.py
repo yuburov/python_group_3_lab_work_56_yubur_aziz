4271dfb35856e37e66ee5c35b18ea299395d1566
Farid <thesomeq@gmail.com>
2019-09-25 20:16:21 +0600

    Added inline Comment admin in articles.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from webapp.models import Article, Comment, Category
 
 
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'text']
+    extra = 0
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'author', 'created_at']
     list_filter = ['author', 'category']
@@ -9,6 +15,7 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['title', 'text']
     exclude = []
     readonly_fields = ['created_at', 'updated_at']
+    inlines = [CommentAdmin]
 
 
 admin.site.register(Article, ArticleAdmin)
